ead7a291e273d3f613df272bdfda434d2c41e2a2
Rychellos <lineardubleclick@gmail.com>
2026-01-19 07:15:11 +0100

    Fixed invalid path in pom.xml cloning of frontend resources Fixed WebConfig's SPA implementation Add missing check in GenericService

--- web-api/src/main/java/pl/rychellos/hotel/webapi/configuration/WebConfig.java
+++ web-api/src/main/java/pl/rychellos/hotel/webapi/configuration/WebConfig.java
@@ -1,32 +1,24 @@
 package pl.rychellos.hotel.webapi.configuration;
 
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class WebConfig {
-
-    @RestControllerAdvice
-    public static class SpaController {
-
-        @ExceptionHandler(HttpClientErrorException.NotFound.class)
-        public Object redirect(HttpServletRequest request, HttpServletResponse response, HttpClientErrorException.NotFound error) {
-            if (request.getMethod().equalsIgnoreCase(HttpMethod.GET.name())) {
-                response.setStatus(HttpStatus.OK.value());
-                return "forward:/index.html";
-            } else {
-                return response;
-            }
+    @Controller
+    public static class SpaFallbackController {
+        @GetMapping(value = {
+            "/",
+            "/{path:^(?!api|_build|_server|favicon\\.ico|icon\\.png|index\\.html).*}",
+            "/{path:^(?!api|_build|_server|favicon\\.ico|icon\\.png|index\\.html).*}/**"
+        })
+        public String forward() {
+            return "forward:/index.html";
         }
     }
 }
