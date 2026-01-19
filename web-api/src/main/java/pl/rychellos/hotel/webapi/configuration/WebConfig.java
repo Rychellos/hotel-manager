997e331cd1fd27a303ca91104ac4b4b2a20aad22
Rychellos <lineardubleclick@gmail.com>
2026-01-19 07:26:40 +0100

    fix formatting issues

--- web-api/src/main/java/pl/rychellos/hotel/webapi/configuration/WebConfig.java
+++ web-api/src/main/java/pl/rychellos/hotel/webapi/configuration/WebConfig.java
@@ -1,11 +1,11 @@
 package pl.rychellos.hotel.webapi.configuration;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
